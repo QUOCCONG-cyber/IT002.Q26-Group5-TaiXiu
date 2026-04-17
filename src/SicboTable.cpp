a9118a6185a81136940abd57393e11bac510532e
dnghia-501 <dnghiatr@gmail.com>
2026-04-17 20:56:31 +0700

    add comment

--- src/SicboTable.cpp
+++ src/SicboTable.cpp
@@ -1,11 +1,11 @@
-#include "Table.h"
+﻿#include "Table.h"
 #include <iostream>
-
+/*tạo bàn có 3 xúc xắc*/
 Table::Table(const House& h, const Player& p)
     : house(h), player(p) {
     dices = std::vector<Dice>(3);
 }
-
+/*tính kết quả*/
 void Table::play() {
     int totalScore = 0;
 
--- src/dice.cpp
+++ src/dice.cpp
@@ -1,10 +1,10 @@
-#include "Dice.h"
-
+﻿#include "Dice.h"
+/*tạo xúc xắc*/
 Dice::Dice() {
     std::random_device rd;
     rng.seed(rd());
 }
-
+/*lắc xúc xắc*/
 int Dice::roll() {
     std::uniform_int_distribution<int> dist(1, 6);
     return dist(rng); 
